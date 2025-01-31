fcc4dd4fbf3efbf9406e1b7c4cfdaeb9ee70cece
Debashish Sarkar <debashishsarkar90072@gmail.com>
2025-01-30 23:35:17 -0800

    Update wsgi.py

--- api_swalook/wsgi.py
+++ api_swalook/wsgi.py
@@ -8,8 +8,8 @@ https://docs.djangoproject.com/en/4.0/howto/deployment/wsgi/
 """
 
 import os
-import pymysql
-pymysql.install_as_MySQLdb()
+# import pymysql
+# pymysql.install_as_MySQLdb()
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'api_swalook.settings')
