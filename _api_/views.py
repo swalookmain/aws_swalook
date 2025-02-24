b2b6210edd5c0527195c81e367f228f1f7e24ff3
Debashish Sarkar <debashishsarkar90072@gmail.com>
2025-02-24 06:59:16 -0800

    Update views.py

--- _api_/views.py
+++ _api_/views.py
@@ -2648,6 +2648,151 @@ class service_category(APIView):
                 'data': None
             }, status=status.HTTP_404_NOT_FOUND)
 
+class service_category(APIView):
+    permission_classes = [IsAuthenticated]
+    serializer_class = VendorProductCategorySerializer
+
+    def __init__(self, **kwargs):
+        self.cache_key = None
+        super().__init__(**kwargs)
+
+    @transaction.atomic
+    def post(self, request):
+        branch_name = request.query_params.get('branch_name')
+
+        if not branch_name:
+            return Response({
+                'success': False,
+                'status_code': status.HTTP_400_BAD_REQUEST,
+                'error': {
+                    'code': 'Bad Request',
+                    'message': 'branch_name parameter is missing!'
+                },
+                'data': None
+            }, status=status.HTTP_400_BAD_REQUEST)
+
+        serializer = self.serializer_class(data=request.data, context={'request': request, 'branch_id': branch_name,})
+
+        serializer.create(validated_data=request.data)
+        return Response({
+            "status": True,
+            "message": "Vendor Product category Added Succesfully"
+        }, status=status.HTTP_201_CREATED)
+
+    def get(self, request):
+        branch_name = request.query_params.get('branch_name')
+        if not branch_name:
+            return Response({
+                'success': False,
+                'status_code': status.HTTP_400_BAD_REQUEST,
+                'error': {
+                    'code': 'Bad Request',
+                    'message': 'branch_name parameter is missing!'
+                },
+                'data': None
+            }, status=status.HTTP_400_BAD_REQUEST)
+
+        data = VendorProductCategory.objects.filter(user=request.user,)
+        serializer = self.serializer_class(data, many=True)
+
+        return Response({
+            "status": True,
+            "data": serializer.data
+        }, status=status.HTTP_200_OK)
+
+    def put(self, request):
+        id = request.query_params.get('id')
+        branch_name = request.query_params.get('branch_name')
+
+        service_obj = VendorProductCategory.objects.filter(user=request.user, vendor_branch_id=branch_name, service_category=request.data.get('category')).exclude(id=id)
+        if service_obj.exists():
+            return Response({
+                'success': False,
+                'status_code': status.HTTP_200_OK,
+                'error': {
+                    'code': 'The request was successful',
+                    'message': 'Service with the same name already exists on this branch!'
+                },
+                'data': None
+            }, status=status.HTTP_200_OK)
+
+        try:
+            service_instance = VendorProductCategory.objects.get(id=id, user=request.user, vendor_branch_id=branch_name)
+        except VendorProductCategory.DoesNotExist:
+            return Response({
+                'success': False,
+                'status_code': status.HTTP_404_NOT_FOUND,
+                'error': {
+                    'code': 'Not Found',
+                    'message': 'product not found!'
+                },
+                'data': None
+            }, status=status.HTTP_404_NOT_FOUND)
+
+        serializer = self.serializer_class(instance=service_instance, data=request.data, context={'request': request, 'branch_id': branch_name})
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response({
+                'success': True,
+                'status_code': status.HTTP_200_OK,
+                'error': {
+                    'code': 'The request was successful',
+                    'message': 'Service updated on this branch!'
+                },
+                'data': None
+            }, status=status.HTTP_200_OK)
+
+        return Response({
+            'success': False,
+            'status_code': status.HTTP_400_BAD_REQUEST,
+            'error': {
+                'code': 'Validation Error',
+                'message': 'Serializer data is invalid!'
+            },
+            'data': serializer.errors
+        }, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request):
+        id = request.query_params.get('id')
+
+        if not id:
+            return Response({
+                'success': False,
+                'status_code': status.HTTP_400_BAD_REQUEST,
+                'error': {
+                    'code': 'Bad Request',
+                    'message': 'ID parameter is missing!'
+                },
+                'data': None
+            }, status=status.HTTP_400_BAD_REQUEST)
+
+        try:
+            queryset = VendorProductCategory.objects.get(id=id, user=request.user)
+            queryset.delete()
+
+            return Response({
+                'success': True,
+                'status_code': status.HTTP_200_OK,
+                'error': {
+                    'code': 'The request was successful',
+                    'message': 'Product Category deleted successfully!'
+                },
+                'data': None
+            }, status=status.HTTP_200_OK)
+
+        except VendorProductCategory.DoesNotExist:
+            return Response({
+                'success': False,
+                'status_code': status.HTTP_404_NOT_FOUND,
+                'error': {
+                    'code': 'Not Found',
+                    'message': 'Service not found!'
+                },
+                'data': None
+            }, status=status.HTTP_404_NOT_FOUND)
+
+
 
 class Table_servicess(APIView):
     permission_classes = [IsAuthenticated]
