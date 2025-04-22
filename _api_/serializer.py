28ce26f1eb0b37cf7a30ed7ea9d70409db406f57
Debashish Sarkar <debashishsarkar90072@gmail.com>
2025-04-22 00:07:24 -0700

    Update serializer.py

--- _api_/serializer.py
+++ _api_/serializer.py
@@ -500,7 +500,12 @@ class staff_attendance_serializer(serializers.Serializer):
 
     
     def update(self, instance, validated_data):
-        validated_data['out_time'] = validated_data.pop('json_data')[0].get('out_time')
+        
+        if self.context.get('type') == "admin":
+            validated_data['out_time'] = validated_data.pop('json_data')[0].get('out_time')
+            validated_data['in_time'] = validated_data.pop('json_data')[0].get('in_time')
+        else:
+            validated_data['out_time'] = validated_data.pop('json_data')[0].get('out_time')
         for attr, value in validated_data.items():
             setattr(instance, attr, value)
 
