9ac25e4b41b29f129755c491374d89c225fc4a38
Debashish Sarkar <debashishsarkar90072@gmail.com>
2025-04-28 23:48:02 -0700

    Update views.py

--- _api_/views.py
+++ _api_/views.py
@@ -708,14 +708,14 @@ class vendor_billing(APIView):
         ).order_by('-date')
        
         invoice_data = billing_serializer_get(queryset, many=True).data
-        for idx, invoice in enumerate(invoice_data):
-                    slno = invoice.get('slno')
-                    if slno:  
-                        invoice_filename = f"Invoice-{slno}.pdf"
-                        invoice_path = os.path.join('media/pdf', invoice_filename)
-                        invoice_data[idx]['pdf_path'] = invoice_path
-                    else:
-                        invoice_data[idx]['pdf_path'] = None  
+        # for idx, invoice in enumerate(invoice_data):
+        #             slno = invoice.get('slno')
+        #             if slno:  
+        #                 invoice_filename = f"Invoice-{slno}.pdf"
+        #                 invoice_path = os.path.join('media/pdf', invoice_filename)
+        #                 invoice_data[idx]['pdf_path'] = invoice_path
+        #             else:
+        #                 invoice_data[idx]['pdf_path'] = None  
 
 
         return Response({
