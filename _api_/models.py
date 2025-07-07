0d07776b209f8b3bdb0e9be7f425ca3dabb76bb3
Debashish Sarkar <debashishsarkar90072@gmail.com>
2025-07-07 08:16:06 -0700

    Update models.py

--- _api_/models.py
+++ _api_/models.py
@@ -676,6 +676,8 @@ class Purchase_entry(models.Model):
     payment_term = models.CharField(max_length=200, null=True, blank=True)
     amount_paid = models.CharField(max_length=200, null=True, blank=True)
     note = models.CharField(max_length=200, null=True, blank=True)
+    image_1 = models.ImageField(upload_to='purchase')
+    image_2 = models.ImageField(upload_to='purchase')
     
 
 
