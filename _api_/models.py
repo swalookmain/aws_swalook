5504913c13f2cc506a3fd2c6299838fb1346ddc8
Debashish Sarkar <debashishsarkar90072@gmail.com>
2025-07-20 21:01:08 -0700

    Update models.py

--- _api_/models.py
+++ _api_/models.py
@@ -331,6 +331,15 @@ class VendorStaff(models.Model):
             models.Index(fields=['vendor_name', 'vendor_branch', 'staff_name']),
         ]
 
+
+class StaffAdvanceModel(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    advance_amount = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
+    vendor_name = models.ForeignKey(User, on_delete=models.CASCADE, null=True, db_index=True)
+    vendor_branch = models.ForeignKey(SalonBranch, on_delete=models.SET_NULL, null=True, db_index=True)
+    staff = models.ForeignKey(VendorStaff, on_delete=models.SET_NULL, null=True, db_index=True)
+    created_at = models.DtaeField()
+    
 class SalesTargetSetting(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     service_target = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
