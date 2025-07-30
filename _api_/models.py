72ccaeffdb60baf6d362894f3681f2e0881a8fd9
Debashish Sarkar <debashishsarkar90072@gmail.com>
2025-07-30 10:14:02 -0700

    Update models.py

--- _api_/models.py
+++ _api_/models.py
@@ -147,7 +147,7 @@ class CustomerCoupon(models.Model):
 
 class VendorCustomers(models.Model):
     id = models.UUIDField(default=uuid.uuid4, primary_key=True, editable=False)
-    # loyality_profile = models.ForeignKey(VendorCustomerLoyalityPoints, on_delete=models.SET_NULL,blank=True,null=True)
+    loyality_profile = models.ForeignKey(VendorCustomerLoyalityPoints, on_delete=models.SET_NULL,blank=True,null=True)
     created_at = models.DateTimeField(auto_now_add=True,blank=True,null=True)
     user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, db_index=True)
     name = models.CharField(max_length=30, blank=True, null=True)
