c21e40540b480cce5edbd6d98e58cf38aed4e81a
Debashish Sarkar <debashishsarkar90072@gmail.com>
2025-08-11 08:57:55 -0700

    Update serializer.py

--- _api_/serializer.py
+++ _api_/serializer.py
@@ -1202,10 +1202,10 @@ class VendorExpenseSerializer(serializers.ModelSerializer):
             validated_data['due_amount'] = int(validated_data.get('expense_amount')) - int(validated_data.get('amount_paid'))
         except Exception:
             validated_data['due_amount'] = 0
-        for i in validated_data.get('inventory_item'):
-            product  = VendorInventoryProduct.objects.get(id=i.get('item'))
-            product.stocks_in_hand+=int(i.get('quantity'))
-            product.save()
+        # for i in validated_data.get('inventory_item'):
+        #     product  = VendorInventoryProduct.objects.get(id=i.get('item'))
+        #     product.stocks_in_hand+=int(i.get('quantity'))
+        #     product.save()
         if validated_data.get('expense_category')[0] == "cf901497-2934-4cb0-b500-e64d103f0e91":
             stf = VendorStaff.objects.get(vendor_branch_id= self.context.get('branch_id'),staff_name=validated_data.get('expense_account'))
             StaffAdvanceModel.objects.create(vendor_name=self.context.get('request').user,vendor_branch_id=self.context.get('branch_id'),advance_amount=validated_data.get('expense_amount'),created_at=validated_data.get("date"),staff=stf)
