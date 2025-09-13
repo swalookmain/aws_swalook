2004694e22fd2f0fbf28153efb6bdff2d25f16aa
Debashish Sarkar <debashishsarkar90072@gmail.com>
2025-09-13 09:13:25 -0700

    Update serializer.py

--- _api_/serializer.py
+++ _api_/serializer.py
@@ -1422,7 +1422,7 @@ class staff_attendance_serializer_update_mobile(serializers.Serializer):
             d_lat = math.radians(lat2 - lat1)
             d_lon = math.radians(lon2 - lon1)
             
-            a = math.sin(d_lat / 2)**2 + math.cos(math.radians(lat1)) * math.cos(math.radians(lat2)) * math.sin(d_lon / 2)**2
+            a = float(math.sin(d_lat / 2)**2 + math.cos(math.radians(lat1))) * float(math.cos(math.radians(lat2)) * math.sin(d_lon / 2))**2
             c = 2 * math.atan2(math.sqrt(a), math.sqrt(1 - a))
     
             return R * c  
